295dcc3e165b61254697ec12a26395981f0b683e
khalinoid <khalinoid@gmail.com>
2025-05-24 03:16:15 +0300

    version update and production deployement

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+
 import { connectDb } from "./config/db.js";
 
 import productRoutes from "./routes/product.route.js";
 
-const app = express();
-app.use(express.json());
-
 dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
-app.listen(PORT, async () => {
-  await connectDb();
-  console.log(`Server Started at Port http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  connectDb();
+  console.log("Server started at http://localhost:" + PORT);
 });
